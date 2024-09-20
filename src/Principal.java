fbbea7665f21ff75dfcd79ee790dbc2b3f01dc1f
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2024-09-20 18:07:40 -0400

    Update code to use encapsulation  add Seria and Titulo to start using OOP

--- src/Principal.java
+++ src/Principal.java
@@ -1,32 +1,28 @@
 import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
 
 public class Principal {
 
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
         miPelicula.setNombre("Encanto");
-        miPelicula.setFecheDeLazamineto(2921);
+        miPelicula.setFechaDeLanzamiento(2021);
         miPelicula.setDuracionEnMinutos(120);
         miPelicula.setIncluidoEnElPlan(true);
 
-       miPelicula.muestraFichaTecnica();
-       miPelicula.evalua( 10);
-       miPelicula.evalua(10);
-       miPelicula.evalua(7.8);
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(10);
+        miPelicula.evalua(10);
+        miPelicula.evalua(7.8);
+        System.out.println("Média de evaluaciones de la película: " +miPelicula.calculaMedia());
 
-       System.out.println(miPelicula.getTotaDeLasEvaluciones());
-       System.out.println(miPelicula.calculaMedia());
-
-
-
-
-
-
-
-//        com.aluracursos.screenmatch.modelos.Pelicula otraPelicula = new com.aluracursos.screenmatch.modelos.Pelicula();
-//        otraPelicula.nombre = "matrix";
-//        otraPelicula.fecheDeLazamineto = 1998;
-//        otraPelicula.duracionEnMinutos = 180;
-
+        Serie casaDragon = new  Serie();
+        casaDragon.setNombre ("La casa del Dragon");
+        casaDragon.setFechaDeLanzamiento(2022);
+        casaDragon.setTemporadas(1);
+        casaDragon.setMinutosPorEpisodio(50);
+        casaDragon.setEpisodiosPorTemporada(10);
+        casaDragon.muestraFichaTecnica();
+        System.out.println(casaDragon.getDuracionEnMinutos());
     }
 }
