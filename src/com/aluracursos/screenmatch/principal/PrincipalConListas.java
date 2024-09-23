fa064d3fbb2e3da40b30a332408b78c8cab1e525
acmcalvo <91921941+acmcalvo@users.noreply.github.com>
2024-09-23 17:28:34 -0400

    updated constructor

---
+++ src/com/aluracursos/screenmatch/principal/PrincipalConListas.java
@@ -0,0 +1,48 @@
+package com.aluracursos.screenmatch.principal;
+
+import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
+import com.aluracursos.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class PrincipalConListas {
+    public static void main(String[] args) {
+        Pelicula miPelicula = new Pelicula("Encanto", 2021);
+        miPelicula.evalua( 9);
+        Pelicula otraPelicula = new Pelicula("Avatar", 2023);
+        otraPelicula.evalua(6);
+        var peliculaDeBruno = new Pelicula("El señor de los anillos", 2001);
+        peliculaDeBruno.evalua(10);
+        Serie lost = new Serie("Lost", 2000);
+
+        ArrayList<Titulo> lista = new ArrayList<>();
+        lista.add(miPelicula);
+        lista.add(otraPelicula);
+        lista.add(lost);
+        lista.add(peliculaDeBruno);
+
+        for (Titulo item: lista) {
+            System.out.println(item.getNombre());
+            if (item instanceof Pelicula pelicula && pelicula.getClasificacion() > 2) {
+                System.out.println(pelicula.getClasificacion());
+
+            }
+        }
+
+
+        ArrayList<String> listaDeArtistas = new ArrayList<>();
+        listaDeArtistas.add("Penelope Cruz");
+        listaDeArtistas.add("Antonio Banderas");
+        listaDeArtistas.add("Ricardo Darin");
+        System.out.println(listaDeArtistas);
+
+        Collections.sort(listaDeArtistas);
+        System.out.println("Lista de artistas ordenada " + listaDeArtistas);
+
+        Collections.sort(lista);
+        System.out.println("Lista de titulos :" + lista);
+
+    }
+}
